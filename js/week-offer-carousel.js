6463a03f8b84e3f726b97f5792aedfb7ccce85ac
vpysanka <32010209+vpysanka@users.noreply.github.com>
2017-11-15 01:46:05 +0200

    Update week-offer-carousel.js

--- js/week-offer-carousel.js
+++ js/week-offer-carousel.js
@@ -1,19 +1,22 @@
 let slideIndex = 0;
 let slides = document.getElementsByClassName('week-offer-block');
+let x = 0;
+let phoneScreen = 599;
 
 const prev = document.querySelector('.wo_prev');
 const next = document.querySelector('.wo_next');
 
-for (let i = 3; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}    
-            
+if (window.innerWidth <= phoneScreen)
+    x = 1;
+else
+    x = 3;
+
 next.addEventListener('click', function() {
-    nextPrev(3);
+    nextPrev(x);
 });
     
 prev.addEventListener('click', function() {
-    nextPrev(-3);
+    nextPrev(-x);
 });
     
 function showSlide() {
@@ -21,25 +24,29 @@ function showSlide() {
         slides[i].style.display = "none";
     }
     
-    if (slides.length - slideIndex >= 3) {
+    if (window.innerWidth <= phoneScreen) {
         slides[slideIndex].style.display = "inline-block";
-        slides[slideIndex + 1].style.display = "inline-block";
-        slides[slideIndex + 2].style.display = "inline-block";
+    } else {
+        if (slides.length - slideIndex >= 3) {
+            slides[slideIndex].style.display = "inline-block";
+            slides[slideIndex + 1].style.display = "inline-block";
+            slides[slideIndex + 2].style.display = "inline-block";
+        }
+        
+        if (slides.length - slideIndex == 2) {
+            slides[slideIndex - 1].style.display = "inline-block";
+            slides[slideIndex].style.display = "inline-block";
+            slides[slideIndex + 1].style.display = "inline-block";
+        }
+        
+        if (slides.length - slideIndex == 1) {
+            slides[slideIndex - 2].style.display = "inline-block";
+            slides[slideIndex - 1].style.display = "inline-block";
+            slides[slideIndex].style.display = "inline-block";
+        }
     }
     
-    if (slides.length - slideIndex == 2) {
-        slides[slideIndex - 1].style.display = "inline-block";
-        slides[slideIndex].style.display = "inline-block";
-        slides[slideIndex + 1].style.display = "inline-block";
-    }
-    
-    if (slides.length - slideIndex == 1) {
-        slides[slideIndex - 2].style.display = "inline-block";
-        slides[slideIndex - 1].style.display = "inline-block";
-        slides[slideIndex].style.display = "inline-block";
-    }
-    
-    if (slides.length - slideIndex <= 3) {
+    if (slides.length - slideIndex <= x) {
         document.getElementsByClassName('wo_next')[0].style.display = "none";
     } else {
         document.getElementsByClassName('wo_next')[0].style.display = "inline-block";
