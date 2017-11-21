d49e8f286d6e80f587dca26f6fbcab58c3418514
vpysanka <32010209+vpysanka@users.noreply.github.com>
2017-11-22 01:22:17 +0200

    Update week-offer-carousel.js

--- js/week-offer-carousel.js
+++ js/week-offer-carousel.js
@@ -1,58 +1,69 @@
-let slides = document.getElementsByClassName('week-offer-block');
-let slideIndex = 0;
-let elements;
+let wo_slides = document.getElementsByClassName('week-offer-block');
+let wo_slideIndex = 0;
+let wo_elements;
+let wo_blockWidth;
 
-const prev = document.querySelector('.wo_prev');
-const next = document.querySelector('.wo_next');
+const wo_prev = document.querySelector('.wo_prev');
+const wo_next = document.querySelector('.wo_next');
 
-next.addEventListener('click', function() {
-    elements = Math.floor(document.querySelector('.week-offer-all-blocks').clientWidth / 300);
-    nextPrev(elements);
+wo_next.addEventListener('click', function() {
+    wo_blockWidthDetermine();
+    wo_elements = Math.floor(document.querySelector('.week-offer-all-blocks').clientWidth / wo_blockWidth);
+    wo_nextPrev(wo_elements);
 });
     
-prev.addEventListener('click', function() {
-    elements = Math.floor(document.querySelector('.week-offer-all-blocks').clientWidth / 300);
-    nextPrev(-elements);
+wo_prev.addEventListener('click', function() {
+    wo_blockWidthDetermine();
+    wo_elements = Math.floor(document.querySelector('.week-offer-all-blocks').clientWidth / wo_blockWidth);
+    wo_nextPrev(-wo_elements);
 });
     
-function showSlide() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function wo_showSlide() {
+    for (let i = 0; i < wo_slides.length; i++) {
+        wo_slides[i].style.display = "none";
     }
 
-    if (slides.length - slideIndex >= elements) {
-        for (let i = 0; i < elements; i++) {
-            slides[slideIndex + i].style.display = "inline-block";
+    if (wo_slides.length - wo_slideIndex >= wo_elements) {
+        for (let i = 0; i < wo_elements; i++) {
+            wo_slides[wo_slideIndex + i].style.display = "inline-block";
         }
     } else {
-        for (let a = 0; a <= (slides.length - 1 - slideIndex); a++) {
-            slides[slideIndex + a].style.display = "inline-block";
+        for (let a = 0; a <= (wo_slides.length - 1 - wo_slideIndex); a++) {
+            wo_slides[wo_slideIndex + a].style.display = "inline-block";
         }
 
-        for (let b = 0; b <= (elements - (slides.length - slideIndex)); b++) {
-            slides[slideIndex - b].style.display = "inline-block";
+        for (let b = 0; b <= (wo_elements - (wo_slides.length - wo_slideIndex)); b++) {
+            wo_slides[wo_slideIndex - b].style.display = "inline-block";
         }
     }
     
-    if (slides.length - slideIndex <= elements) {
-        slideIndex = slides.length - elements;
+    if (wo_slides.length - wo_slideIndex <= wo_elements) {
+        wo_slideIndex = wo_slides.length - wo_elements;
         document.getElementsByClassName('wo_next')[0].style.display = "none";
     } else {
         document.getElementsByClassName('wo_next')[0].style.display = "inline-block";
     }
     
-    if (slideIndex == 0) {
+    if (wo_slideIndex == 0) {
         document.getElementsByClassName('wo_prev')[0].style.display = "none";
     } else {
         document.getElementsByClassName('wo_prev')[0].style.display = "inline-block";
     }
 }
 
-function nextPrev(elements) {
-    slideIndex += elements;
+function wo_nextPrev(wo_elements) {
+    wo_slideIndex += wo_elements;
 
-    if (slideIndex < 0)
-        slideIndex = 0;
+    if (wo_slideIndex < 0)
+        wo_slideIndex = 0;
     
-    showSlide();
+    wo_showSlide();
+}
+
+function wo_blockWidthDetermine() {
+    if (document.querySelector('.week-offer-all-blocks').clientWidth > 979) {
+        wo_blockWidth = 300;
+    } else {
+        wo_blockWidth = 234;
+    }
 }
